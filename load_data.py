87f3b907e96818d1bbed3ff1d5d53f7f8302fb4b
Ayush <ayush474020@gmail.com>
2024-04-08 18:04:42 +0530

    6

--- load_data.py
+++ load_data.py
@@ -110,7 +110,7 @@ def get_random_neg_samples(num=20):
 
     images_batch = torch.stack(images_batch, dim=0)
     # images_batch = images_batch.view(images_batch[0], 1, images_batch[1], images_batch[2])
-    images_batch = images_batch.view(images_batch.size(0), images_batch.size(1), images_batch.size(2))
+    images_batch = images_batch.view(1, images_batch.size(0), images_batch.size(1), images_batch.size(2))
 
     labels_batch = torch.stack(labels_batch, dim=0)
 
