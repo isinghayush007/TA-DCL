288b52e1b5388d1adffdfebde678b76e9cd8d8e8
Ayush <ayush474020@gmail.com>
2024-04-22 13:21:53 +0530

    1

--- models/backbone.py
+++ models/backbone.py
@@ -19,9 +19,9 @@ class MaxVit(nn.Module):
         self.final_layers = nn.Sequential(
             nn.Conv2d(self.base_model.num_features, 2048, kernel_size=1),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(2048, 2048, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(2048, 2048, kernel_size=4, stride=2, padding=1),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(2048, 2048, kernel_size=3, stride=2, padding=1, output_padding=1)
+            nn.ConvTranspose2d(2048, 2048, kernel_size=4, stride=2, padding=1)
         )
 
     def forward(self, x):
